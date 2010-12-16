8c0fb2c9d3579446036c0846f3421acae60acb91
Chris Beaven <smileychris@gmail.com>
2010-12-17 11:34:26 +1300

    Add tests

---
+++ jsonit/tests.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+from unittest import TestCase
+
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.messages.constants import DEFAULT_TAGS
+from django.contrib.messages.storage import base as messages_base
+from django.contrib.messages.storage.session import SessionStorage
+from django.http import HttpRequest
+from django.utils.functional import lazy
+
+from jsonit.http import JSONResponse
+from jsonit.encoder import encode
+
+
+class BaseTest(TestCase):
+
+    def setUp(self):
+        self.request = HttpRequest()
+        self.request.META['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest'
+
+
+class JSONResponseTest(BaseTest):
+
+    def setUp(self):
+        self.request = HttpRequest()
+        self.request.META['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest'
+
+    def test_success(self):
+        response = JSONResponse(self.request)
+        self.assertEqual(
+            response.content,
+            '{"messages": [], "details": {}, "success": true}'
+        )
+
+    def test_not_success(self):
+        response = JSONResponse(self.request, success=False)
+        self.assertEqual(
+            response.content,
+            '{"messages": [], "details": {}, "success": false}'
+        )
+
+    def test_details(self):
+        response = JSONResponse(self.request, details={'test': 1})
+        self.assertEqual(
+            response.content,
+            '{"messages": [], "details": {"test": 1}, "success": true}'
+        )
+
+
+class MessageTest(BaseTest):
+
+    def setUp(self):
+        super(MessageTest, self).setUp()
+        self.request.session = {}
+        self._old_LEVEL_TAGS = messages_base.LEVEL_TAGS
+        messages_base.LEVEL_TAGS = DEFAULT_TAGS
+        self.request._messages = SessionStorage(self.request)
+
+    def tearDown(self):
+        messages_base.LEVEL_TAGS = self._old_LEVEL_TAGS
+
+    def test_messages(self):
+        messages.info(self.request, 'Hello')
+        response = JSONResponse(self.request)
+        self.assertEqual(
+            response.content,
+            '{"messages": [{"message": "Hello", "tags": "info"}], '
+            '"details": {}, "success": true}'
+        )
+
+
+class EncoderTest(TestCase):
+
+    def test_lazy(self):
+        test_msg = lazy(lambda: 'Test!', unicode)
+        self.assertEqual(encode(test_msg()), '"Test!"')
+
+    def test_custom_encoder(self):
+        encode_dt = lambda d: d.strftime('%d %b %Y')
+        self.assertEqual(encode(datetime(1980, 1, 1),
+                                encoders={datetime: encode_dt}),
+                         '"01 Jan 1980"')
