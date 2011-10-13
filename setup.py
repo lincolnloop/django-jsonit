d680d48589353e4b3c165443be697f8c8d08b8c7
bkonkle <brandon@konkle.us>
2011-10-13 11:31:54 -0500

    Added a README

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from distutils.core import setup
 from jsonit import get_version
 
+try:
+    long_description = open('README', 'rt').read()
+except IOError:
+    long_description = ''
+
 setup(
     name='django-jsonit',
     version=get_version(),
     description="A JSON helper library for Django apps.",
-    #long_description=open('README.rst').read(),
+    long_description=long_description,
     author='Chris Beaven',
     author_email='chris@lincolnloop.com',
     license='BSD',
